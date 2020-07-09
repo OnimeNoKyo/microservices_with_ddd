5a82b1dd0f31f58555add9d396d07d913f9aa9af
neilerua <ianni.aurelien@gmail.com>
2020-07-09 19:11:13 +0200

    FIX: missing weight attribute in Part model

--- bounded-contexts/java/src/main/java/p1/myshop/shipping/domain/Part.java
+++ bounded-contexts/java/src/main/java/p1/myshop/shipping/domain/Part.java
@@ -6,10 +6,18 @@ import p1.myshop.catalog.domain.Weight;
 public class Part {
     public final ItemId itemId;
     public final Quantity quantity;
+    private final Weight weight;
 
-    public Part(ItemId itemId, Weight quantity) {
+    public Part(ItemId itemId, Quantity quantity, Weight weight) {
         this.itemId = itemId;
         this.quantity = quantity;
+        this.weight = weight;
+    }
+
+    public Part(ItemId itemId, Weight weight) {
+        this.itemId = itemId;
+        this.weight = weight;
+        quantity = null;
     }
 
     public ItemId itemId() {
